1da7b562547d34812325c2d85912fe282f46aa4b
biglarpour <biglarpour@gmail.com>
2019-01-15 16:08:16 -1000

    Adds php mailer

--- assets/php/send_mail.php
+++ assets/php/send_mail.php
@@ -1,76 +1,67 @@
 <?php
-if(isset($_POST['email'])) {
 
-    // EDIT THE 2 LINES BELOW AS REQUIRED
-    $email_from = "info@elpatronit.com";
+function died($error) {
+    // your error code can go here
+    echo "We are very sorry, but there were error(s) found with the form you submitted. ";
+    echo "These errors appear below.<br /><br />";
+    echo $error."<br /><br />";
+    echo "Please go back and fix these errors.<br /><br />";
+    exit();
+}
 
-    function died($error) {
-        // your error code can go here
-        echo "We are very sorry, but there were error(s) found with the form you submitted. ";
-        echo "These errors appear below.<br /><br />";
-        echo $error."<br /><br />";
-        echo "Please go back and fix these errors.<br /><br />";
-        die();
+try {
+    require_once('./class.phpmailer.php');
+    if(isset($_POST['email'])) {
+
+        $email = $_POST['email'];
+        $name = $_POST['name'];
+        $message = $_POST['message'];
+        $subject = $name . " contacting you via Biglarpour.com";
+
+        $error_message = "";
+        $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
+        $string_exp = "/^[A-Za-z .'-]+$/";
+
+
+        if(!array_key_exists('gotcha', $_POST)) {
+            died("Who do you think you are trying to abuse us.");
+        }
+
+        if(!preg_match($email_exp, $email)) {
+            $error_message .= 'The Email Address you entered does not appear to be valid.<br />';
+        }
+
+        if(!preg_match($string_exp,$name)) {
+            $error_message .= 'The Name you entered does not appear to be valid.<br />';
+        }
+
+        if(strlen($error_message) > 0) {
+            died($error_message);
+        }
+
+        $mail = new PHPMailer();
+        $mail->SMTPDebug  = 0;
+        $mail->SMTPAuth   = true;
+        $mail->SMTPSecure = "ssl";
+        $mail->Host       = "smtp.gmail.com";
+        $mail->Port       = 465;
+        $mail->AddAddress("<insert email to>");
+        $mail->Username= "<insert username>";
+        $mail->Password= "<insert password>";
+        $mail->SetFrom($email, $name);
+        $mail->AddReplyTo($email, $name);
+        $mail->Subject    = $subject;
+        $mail->MsgHTML($message);
+        $mail->Send();
+        header("Location:https://biglarpour.com");
+        exit();
+        }
+    else{
+        echo "No email found in POST" . $_POST;
     }
-
-    $name = $_POST['name']; // required
-    $email_address = $_POST['email']; // required
-    $telephone = $_POST['phone']; // not required
-    $comments = $_POST['message']; // required
-    $company = $_POST['Company'];
-    $email_subject = "Prospect client requesting contact ";
-    $email_subject .= $name;
-    $referred = NULL;
-
-    if(array_key_exists('refer', $_POST)) {
-        $referred = $_POST['refer'];
-    }
-
-    $error_message = "";
-
-    $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
-
-  if(!preg_match($email_exp,$email_address)) {
-    $error_message .= 'The Email Address you entered does not appear to be valid.<br />';
-  }
-
-    $string_exp = "/^[A-Za-z .'-]+$/";
-
-  if(!preg_match($string_exp,$name)) {
-    $error_message .= 'The Name you entered does not appear to be valid.<br />';
-  }
-
-  if(strlen($error_message) > 0) {
-    died($error_message);
-  }
-
-    $email_message = "Personal details below.\n\n";
-
-
-    function clean_string($string) {
-      $bad = array("content-type","bcc:","to:","cc:","href");
-      return str_replace($bad,"",$string);
-    }
-
-
-
-    $email_message .= "Name: ".clean_string($name)."\n";
-    $email_message .= "Email: ".clean_string($email_address)."\n";
-    $email_message .= "Telephone: ".clean_string($telephone)."\n";
-    $email_message .= "Company: ".clean_string($company)."\n";
-    $email_message .= "Comments: ".clean_string($comments)."\n";
-    if(!is_null($referred)){
-        $email_message .= "Referred By: ".clean_string($referred)."\n";
-    }
-
-// create email headers
-$headers = 'From: '.$email_from."\r\n".
-'X-Mailer: PHP/' . phpversion();
-@mail($email_from, $email_subject, $email_message, $headers);
-exit(header("Location:http://www.elpatronit.com/#contact_success"));
-?>
-
-<?php
-
+}
+catch (Exception $e)
+{
+    echo 'Caught exception: ',  $e->getMessage(), "\n";
 }
 ?>
